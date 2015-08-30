b6c19bfe485941fd0e9d7a8ba1b7c73185216be9
wong2 <wonderfuly@gmail.com>
2015-08-30 12:37:21 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pyrenren',
-    version='1.1',
+    version='1.2',
     url='https://github.com/wong2/pyrenren',
     license='MIT',
     author='wong2',
